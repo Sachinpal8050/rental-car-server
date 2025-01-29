d9837301d35afa4167e54bce1b9246e4e0356d8d
sachinKumar8050 <sachinpal8050101@gmail.com>
2025-01-30 01:01:47 +0530

    update

--- src/app.js
+++ src/app.js
@@ -11,6 +11,15 @@ app.use(
   })
 );
 
+const limiter = rateLimit({
+  windowMs: 10 * 1000, // 10 seconds
+  max: 1, // Allow only 1 request per window per IP
+  message: { error: "Too many requests, please try again later." }
+});
+
+// Apply the rate limiter to all routes
+app.use(limiter);
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
@@ -20,6 +29,7 @@ app.use(cookieParser());
 import leadRouter from "./routes/lead.routes.js";
 import ratingRouter from "./routes/rating.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
+import rateLimit from "express-rate-limit";
 
 //routes declaration
 app.use("/api/v1/lead", leadRouter);
