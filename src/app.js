52d7e5cc5abd0b13ac0d7078c3ae52e6763514cf
sachinKumar8050 <sachinpal8050101@gmail.com>
2025-02-01 18:21:01 +0530

    remove for now

--- src/app.js
+++ src/app.js
@@ -11,14 +11,16 @@ app.use(
   })
 );
 
-const limiter = rateLimit({
-  windowMs: 10 * 1000, // 10 seconds
-  max: 1, // Allow only 1 request per window per IP
-  message: { error: "Too many requests, please try again later." }
-});
 
-// Apply the rate limiter to all routes
-app.use(limiter);
+
+// const limiter = rateLimit({
+//   windowMs: 10 * 1000, // 10 seconds
+//   max: 1, // Allow only 1 request per window per IP
+//   message: { error: "Too many requests, please try again later." }
+// });
+
+// // Apply the rate limiter to all routes
+// app.use(limiter);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
